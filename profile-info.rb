e79d5d15d9a9d45ccb1815c2385a0424f821153f
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-11 15:37:53 -0500

    basics.rb normal mode

--- profile-info.rb
+++ profile-info.rb
@@ -1,12 +1,12 @@
-profile = {:name => "Mike", :age => 25, :username => "mepitre430", :city => "Austin", :country => "USA"}
+profile = {name: "Mike", age: 25, username: "mikepitre", city: "Austin", country: "USA"}
 fav_foods = ["Tacos", "Burgers", "Sandwhiches", "Pizza", "BBQ"]
 
-print "Would you like to see the profile info? > "
+print "Would you like to see the profile info? (yes or no) > "
 see_profile = gets.chomp.upcase!
 
 if see_profile == "YES"
   puts profile
-  puts "These are my 5 favorite foods:"
+  puts "Mike's 5 favorite foods are:"
   print fav_foods
 else
   print "Fine, then we'll quit."
