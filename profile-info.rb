b3308e1580aa3b2435541bb8134e9eaa207b9f79
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-11 14:25:17 -0500

    profile hard mode

--- profile-info.rb
+++ profile-info.rb
@@ -1,1 +1,15 @@
+profile = {:name => "Mike", :age => 25, :username => "mepitre430", :city => "Austin", :country => "USA"}
+fav_foods = ["Tacos", "Burgers", "Sandwhiches", "Pizza", "BBQ"]
+
+print "Would you like to see the profile info? > "
+see_profile = gets.chomp.upcase!
+
+if see_profile == "YES"
+  puts profile
+  puts "These are my 5 favorite foods:"
+  print fav_foods
+else
+  print "Fine, then we'll quit."
+end
+
 
